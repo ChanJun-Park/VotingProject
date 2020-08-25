128749fe0ea0f22ebdb759d8d7352b6667b531a5
Junhee <miffy7177@naver.com>
2020-08-25 11:12:28 +0900

    loginpage끗

---
+++ VotingProject/src/main/java/edu/multi/voting/users/UsersController.java
@@ -0,0 +1,87 @@
+package edu.multi.voting.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+// login - post/get , signup - post/get
+
+
+@Controller
+public class UsersController {
+	
+	@Autowired
+	UsersDAO user_dao;
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String loginform() {
+		return "Login";
+	}
+	
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public ModelAndView loginProcess(String user_id, String password) {
+		System.out.println(user_id);
+		System.out.println(password);
+		ModelAndView mv = new ModelAndView();
+		UsersVO vo = new UsersVO();
+		vo=user_dao.validcheck(user_id,password);
+		if(vo.getUser_id()==null) {
+			System.out.println("아이디 존재 X");
+			mv.addObject("validcheck","아이디가 존재하지 않습니다. 회원가입하세요.");
+			mv.setViewName("Login");
+		}
+		else if(vo.getPassword().equals(password)==false) {
+			System.out.println("비밀번호 일치 X");
+			mv.addObject("validcheck","비밀번호가 일치하지 않습니다.");
+			mv.setViewName("Login");
+		}
+		else {
+			System.out.println("성공");
+			mv.addObject("validcheck","success");
+			//메인화면으로 넘어감
+			mv.setViewName("MainPage");
+		}
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/signup",method=RequestMethod.GET)
+	public String signupform() {
+		return "CreateID";
+	}
+	
+	
+	
+	@RequestMapping(value = "/signup",method=RequestMethod.POST)
+	//아직 미완성
+	public ModelAndView signupProcess(UsersVO vo) {
+		ModelAndView mv = new ModelAndView();
+		System.out.println(vo.getPassword());
+		System.out.println(vo.getNickname());
+		System.out.println(vo.getEmail());
+		
+		user_dao.createUser(vo);
+		mv.setViewName("Login");
+		return mv;
+		
+	}
+	
+	@RequestMapping(value = "/dupcheck", method=RequestMethod.POST)
+	public @ResponseBody String dupcheck(@RequestParam("user_id") String user_id) {
+	
+		String dupcheck;
+		dupcheck= user_dao.dupcheck(user_id);
+		System.out.println(dupcheck);
+		return dupcheck;
+		
+	}
+	
+	
+}
+	
+
+
--- VotingProject/src/main/webapp/WEB-INF/views/CreateID.jsp
+++ VotingProject/src/main/webapp/WEB-INF/views/CreateID.jsp
@@ -42,34 +42,85 @@ width:300px;
 
 
 </style>
+
+
 <title>회원가입</title>
 </head>
 <body>
+
+
+
+<script src="/voting/resources/jquery-3.2.1.min.js"></script>
+<script>
+$(document).ready(function(){
+	var check=false;
+	$("#dupcheckbtn").on('click',function(){
+		$.ajax({
+			url:"dupcheck",
+			method:'post',
+			data:{"user_id":$('#user_id').val()},
+			success: function(data){
+				if(data=="NO"){
+					//중복이면 false
+					alert("중복된 아이디입니다.");
+					check = false;
+				}
+				else if(data=="YES"){
+					alert("사용가능한 아이디입니다.");
+					check = true;
+				}
+				else{
+					alert("오류입니다.");
+					check = false;
+				}
+			}
+		})});
+	$("#signupbtn").on('click',function(){
+		if(check==true){
+			document.getElementById('signupform').submit;
+		}
+		else{
+			alert("중복확인 필요합니다.");
+		}
+		
+	});
+	
+	
+	
+	});
+		
+	
+	
+</script>
 <h1><b>회원가입</b></h1>
 <hr>
 <br>
 <br>
+<form action="/voting/signup" id = "signupform" method="POST" >
 <table>
 <tr>
 <td><label for="id"><b>아이디 :&nbsp;</b></label></td>
 <td><input id="user_id" type="text" name="user_id" placeholder="아이디 입력" style="width:130px;" required/>&nbsp;&nbsp;
-<button class="btn log" style ="width: 100px;height:40px; font-size: 16px;">중복확인</button></td>
+<button id = "dupcheckbtn" class="btn log" style ="width: 100px;height:40px; font-size: 16px;">중복확인</button></td>
+
+
+</tr>
+<tr>
+<td><label for="pw"><b>닉네임 :&nbsp;</b></label></td>
+<td><input id="nickname" type="text" name="nickname" placeholder="닉네임 입력" required/></td>
 </tr>
 <tr>
 <td><label for="pw"><b>이메일 :&nbsp;</b></label></td>
-<td><input id="id" type="email" name="user_pass" placeholder="이메일 입력" required/></td>
+<td><input id="email" type="email" name="email" placeholder="이메일 입력" required/></td>
 </tr>
 <tr>
 <td><label for="pw"><b>비밀번호 :&nbsp;</b></label></td>
-<td><input id="id" type="password" name="user_pass" placeholder="비밀번호 입력" required/></td>
-</tr>
-<tr>
-<td><label for="pw"><b>비밀번호 확인 :&nbsp;</b></label></td>
-<td><input id="id" type="password" name="user_pass" placeholder="비밀번호 확인" required/></td>
+<td><input id="password" type="password" name="password" placeholder="비밀번호" required/></td>
 </tr>
 </table>
 <br>
 <br>
-<button class="btn log" onclick="location.href='/VotingProject/MainPage.jsp'">완료</button>
+<button class="btn log" id = "signupbtn">완료</button>
+</form>
 </body>
 </html>
